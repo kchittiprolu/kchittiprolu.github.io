3e5b563e748b0c8585fb82537a921865d24eabbe
kchittiprolu <kchittiprolu@gmail.com>
2017-10-14 23:33:22 -0400

    Updated home page cover and some styling.

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -1,23 +1,23 @@
 		//Function to collapse opened navbar when user click anywhere on the website
-		$(document).click(function (event) {
+		$(document).click(function(event) {
 		    var clickover = $(event.target);
-		    var $navbar = $(".navbar-collapse");               
+		    var $navbar = $(".navbar-collapse");
 		    var _opened = $navbar.hasClass("in");
-		    if (_opened === true && !clickover.hasClass("navbar-toggle")) {      
+		    if (_opened === true && !clickover.hasClass("navbar-toggle")) {
 		        $navbar.collapse('hide');
 		    }
 		});
 
 		//Function for toggling navbar.
-    	$(function () {
-		    $('.toggle').click(function (event) {
+		$(function() {
+		    $('.toggle').click(function(event) {
 		        event.preventDefault();
 		        var target = $(this).attr('href');
 		        $(target).toggleClass('show hidden');
 		    });
 		});
 
-	$('.nav > li a').click(function(e) {
-    var $this = $(this);
-    $this.parent().siblings().removeClass('active').end().addClass('active');
-});
+		$('.nav > li a').click(function(e) {
+		    var $this = $(this);
+		    $this.parent().siblings().removeClass('active').end().addClass('active');
+		});
